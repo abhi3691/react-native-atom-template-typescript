d98728afbfa554e5e68603f378b457189340edc2
abhinand v k <abhinandazokdio@gmail.com>
2022-09-19 22:06:38 +0530

    updated

--- template/App.tsx
+++ template/App.tsx
@@ -1,30 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import ScreenRatio from './src/global_functions/screen_ratio/ScreenRatio'
-import FontRatio from './src/global_functions/font_ratio/FontRatio'
-import customColor from './src/theme/Color'
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import ScreenRatio from './src/global_functions/screen_ratio/ScreenRatio';
+import FontRatio from './src/global_functions/font_ratio/FontRatio';
+import customColor from './src/theme/Color';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle} >Welcome To REACT-NATIVE-ATOM_TEMPLATE</Text>
+      <Text style={styles.textStyle}>WELCOME TO REACT NATIVE ATOM </Text>
     </View>
-  )
-}
-
-export default App
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     height: ScreenRatio.height,
     width: ScreenRatio.width,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   textStyle: {
     fontSize: FontRatio(20),
     color: customColor.black,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  }
-})
+  },
+});
+
+export default App;
