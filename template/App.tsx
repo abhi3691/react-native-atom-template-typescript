34c4efb1d481a43d17fa095b461e35314ce7aa2a
abhinand v k <abhinandazokdio@gmail.com>
2022-09-24 20:52:35 +0530

    updated

--- template/App.tsx
+++ template/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import ScreenRatio from './src/global_functions/screen_ratio/ScreenRatio';
 import FontRatio from './src/global_functions/font_ratio/FontRatio';
 import customColor from './src/theme/Color';
-
+import Fonts from './src/theme/Fonts';
+import ReactIcon from './src/assets/svg_icons/react.svg' 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.textStyle}>WELCOME TO REACT NATIVE ATOM </Text>
+      <ReactIcon height={FontRatio(100)} width={FontRatio(100)} />
     </View>
   );
 };
@@ -22,6 +24,7 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: FontRatio(20),
     color: customColor.black,
+    fontFamily:Fonts.POPPINSBLACK
   },
 });
 
